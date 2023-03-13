21b3a735d76ef6856f624891ccfc02b9f22669c4
HseinKt <hseinkteish1998@gmail.com>
2023-03-14 00:26:13 +0200

    finished the ajax part

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 
 
-// console.log("data first");
+$.ajax({
+    url: 'http://localhost/News-website/news-website-backend/index_api.php',
+    method: 'GET',
+    dataType: 'json',
+    success: (response) => {
+        const DATA = response;
+        console.log("data entered");
+        console.log(DATA);
+        let box = $('#box');
+
+        $.each(DATA, (i, data) => {
+        let h2 = $('<h2>').text(data.title);
+        let p = $('<p>').text(data.body);
+        let h4 = $('<h4>').html('Created by <span>' + data.author_name + '</span>');
+        box.append(h2, p, h4);
+        });
+      
+        console.log("data finished");
+    },
+    error: function(error) {
+        console.log(error);
+    }
+  });
+  
 
 
-
-
-$(document).ready(() => {
-    $.ajax({
-        url: "http://localhost/News-website/news-website-backend/index_api.php",
-        type: "GET",
-        datatype: "JSON",
-        success: (data) => {
-            let box = $('#box');
-            $.each(data,(index,news)=>{
-                const title = $('<h2></h2>').text(news.title);
-                const body = $('<p></p>').text(news.body);
-                const author = $('<h4>created by </h4>').text(news.author_name);
-                box.append(title,body,author);
-            })
-        },
-        error: (xhr, status, error) => {
-			console.log(error);
-		}
-    });
-})
-
-
-{/* <h2>title</h2>
-            <p>Short paragraph about sport contains all the important and useful information about sport and its importance to the individual and society . Here we will offer you a short paragraph about sport that contains all the information you are looking for.</p>
-            <h4>created by <span>hsein</span></h4> */}
